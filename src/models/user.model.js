64783612548e0baff043e2be993b412ccf8f50a9
Sagar-softechure <136797867+Sagar-softechure@users.noreply.github.com>
2024-12-30 12:46:02 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,4 +60,25 @@ userSchema.methods.isPassworddCorrect = async function (password){
     return await bcrypt.compare(password,this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
 export const User = mongoose.model("User",userSchema)
