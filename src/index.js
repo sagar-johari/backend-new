34b47452a6b74bd5c2ae46a4c9ec19d8b79f6535
Sagar Johari <joharisagar53@gmail.com>
2025-01-02 00:05:45 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,47 +1,25 @@
-// require('dotenv').config({path:'./env'});
+// Import dependencies
+import express from "express";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+
+// Configure environment variables
 dotenv.config({
-    path:'./env'
+    path: './env',
 });
 
+// Initialize Express app
+const app = express();
+
+// Connect to the database and start the server
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at${process.env.PORT}`);
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Mongo db connection failed!!", err);
     });
-})
-.catch((err)=>{
-    console.log("Mongo db connection failed!!",err);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*(async()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    } catch (error) {
-        console.error("ERROR:",error);
-        throw error;
-    }
-})()*/
